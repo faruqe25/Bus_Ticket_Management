d3f7b1120eab8ba118791aa8035784a6ff8fc56c
faruqe25 <faruqe25@gmail.com>
2019-11-22 17:14:29 +0600

    Initial State of Schedule Management

---
+++ BlankSpace/Controllers/BusScheduleController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlankSpace.Database;
+using BlankSpace.Models;
+using BlankSpace.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlankSpace.Controllers
+{
+    public class BusScheduleController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public BusScheduleController(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IActionResult AddSchedule()
+        { 
+            return View();
+        } 
+        [HttpPost]
+        public IActionResult AddSchedule(ScheduleVm a)
+        {
+            Schedule s = new Schedule()
+            {
+                ScheduleId = 0,
+                Destination = a.Destination,
+                StartingFrom = a.StartingFrom,
+                Time = a.Time,
+
+            };
+            _context.Schedules.Add(s);
+            _context.SaveChanges();
+
+            return RedirectToAction("ScheduleList");
+        }
+        public IActionResult ScheduleList()
+        {
+            var s = _context.Schedules.AsNoTracking().ToList();
+            var sent = new List<ScheduleVm>();
+            int c = 1;
+            foreach (var item in s)
+            {
+                ScheduleVm sp = new ScheduleVm()
+                {
+                    ScheduleId = item.ScheduleId,
+                    Destination = item.Destination,
+                    StartingFrom = item.StartingFrom,
+                    Time = item.Time,
+                    Serial = c
+
+                };
+                sent.Add(sp);
+                  c++;
+            }
+           
+           
+
+            return View(sent);
+        }
+    }
+}
---
+++ BlankSpace/ViewModels/ScheduleVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlankSpace.ViewModels
+{
+    public class ScheduleVm
+    {
+        public int ScheduleId { get; set; }
+        public int Serial { get; set; }
+        public string StartingFrom { get; set; }
+        public string Destination { get; set; }
+        public string Time { get; set; }
+
+    }
+}
