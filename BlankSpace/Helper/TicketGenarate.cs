09bbfdb4145b074905ee58d86585a647fa3c6c25
faruqe25 <faruqe25@gmail.com>
2019-11-22 12:24:37 +0600

    Second

---
+++ BlankSpace/Helper/TicketGenarate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Helper
+{
+    public static class TicketGenarate
+    {
+       public static List<BusTicketBooking>Ticket(int SeatRow,int BusId,string Time)
+        { 
+        List<BusTicketBooking> BusTicketBooking = new List<BusTicketBooking>();
+            for (int i = 0; i < SeatRow; i++)
+            {
+                var SeatText = 65;
+                for (int j = 0; j < 4; j++)
+                {
+                    BusTicketBooking vs = new BusTicketBooking()
+                    {
+                        BookingStatus = false,
+                        BusTicketBookingId = 0,
+                        BusId = BusId,
+                        DateTime = Time
+                    };
+                    var t = (char)SeatText;
+                    vs.SeatNumber = t.ToString();
+                    BusTicketBooking.Add(vs);
+                }
+                SeatText++;
+                
+               
+            }
+
+            return BusTicketBooking;
+        
+    }
+}
+}
